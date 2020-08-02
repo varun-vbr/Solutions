46657c110b874104e6e1ed4bbb8edff85185cdde
Varun <varun.vbr@gmail.com>
2020-08-02 23:13:56 +0530

    Initial UI Layout

--- UI/solutions-ui/src/app/app.module.ts
+++ UI/solutions-ui/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SolutionsComponent } from './solutions/solutions.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SolutionsComponent
+  ],
+  exports:[
+    
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
